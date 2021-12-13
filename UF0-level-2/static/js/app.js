e9f6890cc610d84a6e3395e586a226b54058bec7
Daniel Kogel <dkogel123@gmail.com>
2021-12-12 19:41:44 -0600

    multiple filters now functioning

--- UF0-level-2/static/js/app.js
+++ UF0-level-2/static/js/app.js
@@ -5,7 +5,9 @@ let tableData = data;
 // D3 selections
 let tbody = d3.select("tbody");
 
-let button = d3.select("#filter-btn");
+let filterbutton = d3.select("#filter-btn");
+
+let resetButton = d3.select("#reset-btn")
 
 let form = d3.select("#form1");
 
@@ -25,11 +27,34 @@ tableData.forEach(function(sightings)
 );
 
 //event handlers
-button.on("click", runFilter);
+filterbutton.on("click", runFilter);
+resetButton.on("click", runReset);
 form.on("submit", runFilter);
 
-function runFilter() 
-{
+function runReset(){
+
+    d3.event.preventDefault();
+
+    //iniatialize the table
+    tableData.forEach(function(sightings)
+    {
+        let row = tbody.append("tr");
+        Object.entries(sightings).forEach(function([key,value])
+        {
+            let cell = row.append("td");
+            cell.text(value);
+        }
+        );
+
+    }
+    );
+    
+    // clear input fields
+    document.querySelector("form").reset();
+    
+}
+
+function runFilter() {
     d3.event.preventDefault();
 
     //get raw html input
@@ -53,10 +78,68 @@ function runFilter()
     console.log(shapeInput)
 
 
+    
+
+    //clear table
     tbody.selectAll("tr").remove()
 
-    let filteredData = tableData.filter(o => o.datetime == dateInput);
+    //reset filteredData
+    let filteredData = tableData
+
+    //filter data
+    
+    
+    // filteredData = filteredData.filter(o => o.city == cityInput);
+    // filteredData = filteredData.filter(o => o.state == stateInput);
+    // filteredData = filteredData.filter(o => o.country == countryInput);
+    // filteredData = filteredData.filter(o => o.shape == shapeInput);
+
+   switch(dateInput) {
+        case "":
+           console.log("no date selected");
+        break;
+        default:
+            filteredData = filteredData.filter(o => o.datetime == dateInput);
+   }
+
+
+    switch(cityInput) {
+        case "":
+            console.log("no city selected");
+        break;
+        default:
+            filteredData = filteredData.filter(o => o.city == cityInput);
+    } 
+
+
+    switch(stateInput) {
+        case "":
+            console.log("no state selected");
+        break;
+        default:
+            filteredData = filteredData.filter(o => o.state == stateInput);
+    } 
+        
+    switch(countryInput){
+        case "":
+            console.log("no country selected");
+        break;
+        default:
+            filteredData = filteredData.filter( o => o.country == countryInput);
+    }
+
+
+    switch(shapeInput){
+        case "":
+            console.log("no shape selected");
+        break;
+        default:
+            filteredData = filteredData.filter( o => o.shape == shapeInput);
+    }
    
+
+
+    //add filtered data back to table
     filteredData.forEach(function(sightings)
     {
         let row = tbody.append("tr");
